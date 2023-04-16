979fdaa336c5d32139b1d4d297a4823da264d286
hiroyu-n <hiroyukinakatsuka@gmail.com>
2023-04-16 11:02:47 +0900

    ARRAY_CONCAT('[]', '[]')->'[]' bug fix.

--- tests/test_array_concat.py
+++ tests/test_array_concat.py
@@ -15,10 +15,28 @@ def install()-> None:
     pass
 
 class TestArrayConcat(BTC):
-    @pytest.mark.parametrize(("title", "arr", "start", "stop", "step", "expected"), (
-        ("Array.concat(NULL, NULL) -> null", None, None, None),
+    @pytest.mark.parametrize(("title", "arr1", "arr2", "expected"), (
+        ("Array.concat(null, null) -> null", None, None, None),
+        ("Array.concat([1, 2, 3], null)", dumps([1, 2, 3]), None, None),
+        ("Array.concat(null, [1, 2, 3])", None, dumps([1, 2, 3]), None),
+        ("Array.concat([], []) -> []", dumps([]), dumps([]), dumps([])),
+        ("Array.concat(null, []) -> null", None, dumps([]), None),
+        ("Array.concat([], null) -> null", dumps([]), None, None),
     ))
-    def test_simple(self, title: str, arr1: Sequence[Any], arr2: Sequence[Any], expected: Sequence[Any])-> None:
+    def test_null_boundary(self, title: str, arr1: Sequence[Any], arr2: Sequence[Any], expected: Sequence[Any])-> None:
+        cur = self.get_cursor()
+        stmt: str = cur.mogrify("SELECT ARRAY_CONCAT(%(arr1)s, %(arr2)s) AS `expected`;", locals())
+        cur.execute(stmt)
+        raw: str = cur.fetchone()['expected']
+        assert expected == raw, stmt
+
+    @pytest.mark.parametrize(("title", "arr1", "arr2", "expected"), (
+        ("Array.concat([], [1, 2, 3]) -> [1, 2, 3]", dumps([]), dumps([1, 2, 3]), dumps([1, 2, 3])),
+        ("Array.concat([1, 2, 3], []) -> [1, 2, 3]", dumps([]), dumps([1, 2, 3]), dumps([1, 2, 3])),
+        ("Array.concat([1, 2, 3], [4, 5, 6]) -> [1, 2, 3, 4, 5, 6]", dumps([1, 2, 3]), dumps([4, 5, 6]), dumps([1, 2, 3, 4, 5, 6])),
+        ("Array.concat([4, 5, 6], [1, 2, 3]) -> [4, 5, 6, 1, 2, 3]", dumps([4, 5, 6]), dumps([1, 2, 3]), dumps([4, 5, 6, 1, 2, 3])),
+    ))
+    def test_simple_number_element(self, title: str, arr1: Sequence[Any], arr2: Sequence[Any], expected: Sequence[Any])-> None:
         cur = self.get_cursor()
         stmt: str = cur.mogrify("SELECT ARRAY_CONCAT(%(arr1)s, %(arr2)s) AS `expected`;", locals())
         cur.execute(stmt)
